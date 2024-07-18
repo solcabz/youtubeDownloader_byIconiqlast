7a1a36c75251f1a7e39d1f802995a22e16603ce1
solcabz <xhzoul30@gmail.com>
2024-07-18 18:08:37 +0800

    set index

--- server/app.js
+++ server/app.js
@@ -34,6 +34,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Parse JSON bodies
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+
 // Routes
 app.use("/", indexRouter);
 app.use("/auth", authRouter); // Authentication routes
