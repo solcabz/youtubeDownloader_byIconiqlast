1fe1dffa999b0fb4f796a219b9d0075309ed0b0c
solcabz <xhzoul30@gmail.com>
2024-07-19 12:32:42 +0800

    setCors

--- server/app.js
+++ server/app.js
@@ -1,19 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const path = require("path");
 const dotenv = require("dotenv");
 
-// Import the connectDB function and routes
-const { connectDB } = require("./config/dbConnection");
-const indexRouter = require("./routes/root");
-const userRoutes = require("./routes/users");
+//routes connection
+const { connectDB } = require("./config/dbConnection"); // Import the connectDB function
+const indexRouter = require("./routes/root"); // root access for api
+const userRoutes = require("./routes/users"); // Import user routes
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const downloadRouter = require("./routes/download");
-
-// Import custom CORS middleware
-const allowCors = require("./middleware/allowCors");
 
 // Load environment variables from ./config/.env file
 dotenv.config({ path: path.resolve(__dirname, "./config/.env") });
@@ -21,22 +19,29 @@ dotenv.config({ path: path.resolve(__dirname, "./config/.env") });
 const app = express();
 const PORT = 3000;
 
-// Middleware setup
+// Middleware
+const corsOptions = {
+  origin: "http://localhost:5173", // Replace with your frontend URL
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Corrected HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // Handle preflight requests
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Parse JSON bodies
-app.use(express.static(path.join(__dirname, "public"))); // Serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
-// Apply CORS middleware to all routes
-app.use(allowCors);
-
-// Route setup
+// Routes
 app.use("/", indexRouter);
-app.use("/auth", authRouter);
-app.use("/users", userRoutes);
-app.use("/profile", profileRouter);
-app.use("/download", downloadRouter);
+app.use("/auth", authRouter); // Authentication routes
+app.use("/users", userRoutes); // Mount user routes
+app.use("/profile", profileRouter); // Mount profile routes
+app.use("/download", downloadRouter); // Mount Donload Routes
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -44,6 +49,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: "Internal Server Error" });
 });
 
+// Start the server and connect to the database
 // Function to start the server and connect to the database
 const startServer = async () => {
   try {
