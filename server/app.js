de34ec464d60b97df2fc7bea31849d0f7e6c1f56
solcabz <xhzoul30@gmail.com>
2024-07-19 17:24:45 +0800

    new cors origin

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const PORT = 3000;
 
 // Middleware
 const corsOptions = {
-  origin: "https://iconiqlast-downloader.netlify.app", // Replace with your frontend URL
+  origin: "https://iconiqlast-downloader.vercel.app", // Replace with your frontend URL
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Corrected HTTP methods
